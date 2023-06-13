39485e93d8b8f7918bc0f08be01ea9f223a6e4fc
GabrielHJLC <testegabriel483@gmail.com>
2023-06-12 21:24:00 -0300

    conexao

--- api/php/conexao.php
+++ api/php/conexao.php
@@ -1,5 +1,9 @@
 <?php 
-require '../../vendor/autoload.php';
+
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $dbHost = getenv('DB_HOST');
 $dbUser = getenv('DB_USER');
@@ -7,5 +11,11 @@ $dbPassword = getenv('DB_PASSWORD');
 $dbName = getenv('DB_NAME');
 
 $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+if(!$conn){
+    echo 'error' . $conn->error_log;
+}else{
+    echo '....';
+}
 
 ?>
